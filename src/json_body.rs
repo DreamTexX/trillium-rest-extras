efe8ac680eab5659572595409e1108a462e86d23
DreamTexX <kevin.cebulla@dreamtexx.email>
2024-04-11 18:56:28 +0000

    chore: use conn_try macro instead of match internally

--- src/json_body.rs
+++ src/json_body.rs
@@ -2,64 +2,45 @@ use std::error::Error;
 
 use serde::de::DeserializeOwned;
 use serde::Serialize;
-use serde_json::json;
 use trillium::{Conn, KnownHeaderName, Status};
 
-use crate::error::{ConnApiErrorExt, JSON_SERIALIZE_ERROR};
+use crate::conn_try;
 
 #[async_trait::async_trait]
 pub trait JsonBodyConnExt {
     fn json<V: Serialize>(self, value: V) -> Self;
     #[allow(dead_code)]
     fn with_json<V: Serialize>(self, value: V) -> Self;
-    async fn request_body_json<T: DeserializeOwned>(
-        &mut self,
-    ) -> Result<T, Box<dyn Error>>;
+    async fn request_body_json<T: DeserializeOwned>(&mut self) -> Result<T, Box<dyn Error>>;
 }
 
 #[async_trait::async_trait]
 impl JsonBodyConnExt for Conn {
     fn json<V: Serialize>(self, value: V) -> Self {
-        match serde_json::to_string(&value) {
-            Ok(value) => {
-                self.with_status(Status::Ok)
-                    .with_response_header(
-                        KnownHeaderName::ContentType,
-                        "application/json",
-                    )
-                    .with_body(value)
-                    .halt()
-            }
-            Err(e) => {
-                self.error(
-                    JSON_SERIALIZE_ERROR
-                        .with_data(json!({ "innerError": e.to_string() })),
-                )
-            }
-        }
+        let serialized = conn_try!(
+            serde_json::to_string(&value),
+            self,
+            crate::error::JSON_SERIALIZE_ERROR
+        );
+
+        self.with_status(Status::Ok)
+            .with_response_header(KnownHeaderName::ContentType, "application/json")
+            .with_body(serialized)
+            .halt()
     }
 
     fn with_json<V: Serialize>(self, value: V) -> Self {
-        match serde_json::to_string(&value) {
-            Ok(value) => {
-                self.with_response_header(
-                    KnownHeaderName::ContentType,
-                    "application/json",
-                )
-                .with_body(value)
-            }
-            Err(e) => {
-                self.error(
-                    JSON_SERIALIZE_ERROR
-                        .with_data(json!({ "innerError": e.to_string() })),
-                )
-            }
-        }
+        let serialized = conn_try!(
+            serde_json::to_string(&value),
+            self,
+            crate::error::JSON_SERIALIZE_ERROR
+        );
+
+        self.with_response_header(KnownHeaderName::ContentType, "application/json")
+            .with_body(serialized)
     }
 
-    async fn request_body_json<T: DeserializeOwned>(
-        &mut self,
-    ) -> Result<T, Box<dyn Error>> {
+    async fn request_body_json<T: DeserializeOwned>(&mut self) -> Result<T, Box<dyn Error>> {
         let body = self.request_body().await;
         let bytes = body.read_bytes().await?;
         Ok(serde_json::from_slice(&bytes)?)
